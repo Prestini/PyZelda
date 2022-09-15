44a30f93b43469be9a331b88a8aa38f855b6b920
Lucas Martins Prestini <37966787+Prestini@users.noreply.github.com>
2022-09-15 01:22:27 -0300

    Weapons

--- src/level.py
+++ src/level.py
@@ -5,7 +5,36 @@ from settings import *
 from support import import_csv_layout, import_folder
 from tile import Tile
 from debug import debug
+from weapon import Weapon
 
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self) -> None:
+        
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+        # creating the floor
+        self.floor_surf = pygame.image.load('./assets/graphics/tilemap/ground.png').convert_alpha()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
+    def custom_draw(self, player) -> None:
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
+        # draw all of our elements
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
 
 class Level:
     def __init__(self) -> None:
@@ -16,6 +45,9 @@ class Level:
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+
+        # attack sprites
+        self.current_attack = None
 
         # sprite setup
         self.create_map()
@@ -50,39 +82,18 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000,1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
     def run(self) -> None:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug(self.player.status)
-
-class YSortCameraGroup(pygame.sprite.Group):
-    def __init__(self) -> None:
-        
-        # general setup
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2()
-
-        # creating the floor
-        self.floor_surf = pygame.image.load('./assets/graphics/tilemap/ground.png')
-        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
-
-    def custom_draw(self, player) -> None:
-
-        # getting the offset
-        self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_height
-
-        # drawing the floor
-        floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surf, floor_offset_pos)
-
-        # draw all of our elements
-        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
